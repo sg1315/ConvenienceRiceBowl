d4a4ef9ea766f863a7d8a40b0de4110b17060d43
DESKTOP-P7SCEEU\user1 <skskcsj@naver.com>
2025-04-02 17:08:40 +0900

    update- 파일명 변경

--- src/main/java/com/kh/project/cse/boot/controller/HomeController.java
+++ src/main/java/com/kh/project/cse/boot/controller/HomeController.java
@@ -15,90 +15,90 @@ public class HomeController {
     //택봉-발주
     @RequestMapping("/spot_order")
     public String spot_order() {
-        return "spot/ordering";
+        return "order";
     }
     //택봉
 
     //성진 본사-발주 관리
     @RequestMapping("/head_order1")
     public String home3() {
-        return "head_office/order";
+        return "headOrder";
     }
     //성진
 
     //성진 본사-공지사항
     @RequestMapping("/head_announcement")
-    public String head_announcement() { return "head_office/announcement";}
+    public String head_announcement() { return "headAnnouncement";}
     //성진
 
     //성진 본사-상품관리
     @RequestMapping("/head_product")
-    public String head_product() { return "head_office/product";}
+    public String head_product() { return "headProduct";}
     //
 
     //성진 본사-상품관리
     @RequestMapping("/head_store")
-    public String head_store() { return "head_office/store";}
+    public String head_store() { return "headStore";}
     //
     //성진 본사-상품관리
     @RequestMapping("/head_member")
-    public String head_member() { return "head_office/member";}
+    public String head_member() { return "headMember";}
     //
 
     //승기 지점-대시보드
-    @RequestMapping("/dashboard")
-    public String dashboard() {
-        return "spot/dashboard";
+    @RequestMapping("/spot_dashboard")
+    public String spot_dashboard() {
+        return "spotDashboard";
     }
     //승기
 
     //승기 지점-상품목록
-    @RequestMapping("/spotProduct")
-    public String spotProduct() {
-        return "spot/product";
+    @RequestMapping("/spot_product")
+    public String spot_product() {
+        return "spotProduct";
     }
     //승기
 
     //승기 지점-출고
-    @RequestMapping("/spotOutput")
-    public String spotOutput() {
-        return "spot/output";
+    @RequestMapping("/spot_output")
+    public String spot_output() {
+        return "spotOutput";
     }
     //승기
 
     //승기 지점-매출 연단위
-    @RequestMapping("/spotSalesYear")
-    public String index() {
-        return "spot/salesYear";
+    @RequestMapping("/spot_salesYear")
+    public String spot_salesYear() {
+        return "spotSalesYear";
     }
 
     //택봉-직원관리
     @RequestMapping("/spot_member")
     public String spot_member() {
-        return "spot/member";
+        return "spotMember";
     }
     //택봉
 
     //인혜
-    @RequestMapping("/spot-attendance")
-    public String attendance() {
-        return "spot/attendance";
+    @RequestMapping("/spot_attendance")
+    public String spot_attendance() {
+        return "spotAttendance";
     }
 
 
     //인혜
-    @RequestMapping("/spot-inventory")
-    public String inventory() {
-        return "spot/inventory";
+    @RequestMapping("/spot_inventory")
+    public String spot_inventory() {
+        return "spotInventory";
     }
 
-    @RequestMapping("/spot-notice")
-    public String announcement() {
-        return "spot/notice";
+    @RequestMapping("/spot_notice")
+    public String spot_notice() {
+        return "spotNotice";
     }
-    @RequestMapping("/spot-sales")
-    public String sales() {
-        return "spot/sales";
+    @RequestMapping("/spot_sales")
+    public String spot_sales() {
+        return "spotSales";
     }
 
 
@@ -114,7 +114,7 @@ public class HomeController {
     //성진-유통기한관리
     @RequestMapping("/spot_expiration")
     public String spot_expiration() {
-        return "spot/expiration";
+        return "sporExpiration";
     }
     //성진
 
@@ -122,7 +122,7 @@ public class HomeController {
 
     @RequestMapping("/inputmodal")
     public String inputmodal() {
-        return "spot/inputmodal";
+        return "spotInputmodal";
     }
 
     @RequestMapping("/receiving")
