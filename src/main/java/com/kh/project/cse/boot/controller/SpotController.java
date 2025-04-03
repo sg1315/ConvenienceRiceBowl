af6b1d037f1f19d230d79d1488c7e298d7cf9a89
DESKTOP-P7SCEEU\user1 <skskcsj@naver.com>
2025-04-03 10:12:49 +0900

    upadate-지점 컨트롤러 add-서비스(지점)

--- src/main/java/com/kh/project/cse/boot/controller/SpotController.java
+++ src/main/java/com/kh/project/cse/boot/controller/SpotController.java
@@ -5,69 +5,65 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SpotController {
 
-    //승기 지점-대시보드
+    //대시보드
     @RequestMapping("/spot_dashboard")
     public String spot_dashboard() {
         return "spot/spotDashboard";
     }
-    //승기
 
-    //승기 지점-상품목록
+    //상품목록
     @RequestMapping("/spot_product")
     public String spot_product() {
         return "spot/spotProduct";
     }
-    //승기
 
-    //승기 지점-출고
+    //출고
     @RequestMapping("/spot_output")
     public String spot_output() {
         return "spot/spotOutput";
     }
-    //승기
 
-    //승기 지점-매출 연단위
+    //년매출
     @RequestMapping("/spot_salesYear")
     public String spot_salesYear() {
         return "spot/spotSalesYear";
     }
 
-    //택봉-직원관리
+    //멤버관리
     @RequestMapping("/spot_member")
     public String spot_member() {
         return "spot/spotMember";
     }
-    //택봉
 
-    //인혜
+    //근태관리
     @RequestMapping("/spot_attendance")
     public String spot_attendance() {
         return "spot/spotAttendance";
     }
 
-
-    //인혜
+    //재고
     @RequestMapping("/spot_inventory")
     public String spot_inventory() {
         return "spot/spotInventory";
     }
 
+    //공지사항
     @RequestMapping("/spot_notice")
     public String spot_notice() {
         return "spot/spotNotice";
     }
+
+    //월매출
     @RequestMapping("/spot_sales")
     public String spot_sales() {
         return "spot/spotSales";
     }
 
-    //성진-유통기한관리
+    //유통기한
     @RequestMapping("/spot_expiration")
     public String spot_expiration() {
         return "spot/sporExpiration";
     }
-    //성진
-
 
 
     @RequestMapping("/inputmodal")
@@ -80,11 +76,11 @@ public class SpotController {
         return "receiving/receivingDetailsForm";
     }
 
-    //택봉-발주
+    //입고
     @RequestMapping("/spot_order")
     public String spot_order() {
         return "spot/spotOrder";
     }
-    //택봉
+
 
 }
