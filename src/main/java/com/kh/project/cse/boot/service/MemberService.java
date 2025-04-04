8b2a38a27cd9a39d3710535fbae61ab50b4a556c
Ksg <Tropicana1235>
2025-04-04 16:57:03 +0900

    update -  회원가입 insert param 지원진거 수정

--- src/main/java/com/kh/project/cse/boot/service/MemberService.java
+++ src/main/java/com/kh/project/cse/boot/service/MemberService.java
@@ -1,13 +1,14 @@
 package com.kh.project.cse.boot.service;
 
 import com.kh.project.cse.boot.domain.vo.Member;
+import org.apache.ibatis.annotations.Param;
 
 public interface MemberService {
     int insertMember(Member member);
 
-    Member loginMember(String memberId);
+    Member loginMember(@Param("memberId") String memberId);
 
-    int idCheck(String checkId);
+    int idCheck(@Param("checkId") String checkId);
 
-    int insertStore(String storeName);
+    int insertStore(@Param("storeName") String storeName);
 }
